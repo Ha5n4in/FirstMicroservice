8f824da9d29e3c0be0f05713c3f0ab177f5fc4f4
Hasnain <hasnain_zahoor@yahoo.co.uk>
2022-06-18 18:29:33 +0100

    selenium paralysed

--- src/test/java/com/tsi/hasnain/zahoor/program/seleniumTest.java
+++ src/test/java/com/tsi/hasnain/zahoor/program/seleniumTest.java
@@ -1,34 +1,34 @@
-package com.tsi.hasnain.zahoor.program;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import java.time.Duration;
-public class seleniumTest {
-
-    public WebDriver driver;
-    @Test
-    public void chromeDriverTest(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver= new ChromeDriver();
-        driver.quit();
-    }
-
-    @Test
-    public void chromeSessionTest() {
-        System.setProperty("webdriver.chrome.driver", "C:\\React\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        driver = new ChromeDriver(options);
-        driver.quit();
-    }
-
-    @Test
-    public void checkWebsiteConnection(){
-        System.setProperty("webdriver.chrome.driver", "C:\\React\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("http://localhost:3000/");
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-        driver.quit();
-    }
-}
+//package com.tsi.hasnain.zahoor.program;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.junit.jupiter.api.Test;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import java.time.Duration;
+//public class seleniumTest {
+//
+//    public WebDriver driver;
+//    @Test
+//    public void chromeDriverTest(){
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver= new ChromeDriver();
+//        driver.quit();
+//    }
+//
+//    @Test
+//    public void chromeSessionTest() {
+//        System.setProperty("webdriver.chrome.driver", "C:\\React\\chromedriver.exe");
+//        ChromeOptions options = new ChromeOptions();
+//        driver = new ChromeDriver(options);
+//        driver.quit();
+//    }
+//
+//    @Test
+//    public void checkWebsiteConnection(){
+//        System.setProperty("webdriver.chrome.driver", "C:\\React\\chromedriver.exe");
+//        driver = new ChromeDriver();
+//        driver.get("http://localhost:3000/");
+//        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+//        driver.quit();
+//    }
+//}
